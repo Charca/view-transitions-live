468c3ef60cb5de4149bf4e9d58d32c429cf58916
Maxi Ferreira <charca@gmail.com>
2024-04-14 23:18:18 -0700

    WIP using the Navigo router instead of the Navigation API to test in Safari

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,3 +1,5 @@
+import Navigo from 'navigo'
+
 import {
   shouldNotIntercept,
   updateTheDOMSomehow,
@@ -8,105 +10,192 @@ import {
 
 let prevPageScroll = 0
 
-navigation.addEventListener('navigate', (navigateEvent) => {
-  if (shouldNotIntercept(navigateEvent)) return
+const router = new Navigo('/')
+let lastMatch = null
 
-  const toUrl = new URL(navigateEvent.destination.url)
-  const toPath = toUrl.pathname
-  const fromPath = location.pathname
+router.on('/playlist/:id', async (match) => {
+  const { id } = match.data
+  lastMatch = match
 
-  if (location.origin !== toUrl.origin) return
+  const response = await fetch('/fragments/playlist/' + id)
+  const data = await response.text()
 
-  if (toPath.indexOf('/playlist') === 0) {
-    return handlePlaylistTransition(navigateEvent, toPath, fromPath)
-  } else {
-    return handleHomeTransition(navigateEvent, toPath, fromPath)
+  if (!document.startViewTransition) {
+    updateTheDOMSomehow(data)
+    document.documentElement.scrollTop = 0
+    return
   }
+
+  // Make sure there are no other cards with the 'with-transition' class
+  document
+    .querySelectorAll('.card')
+    .forEach((card) => card.classList.remove('with-transition'))
+
+  const card = findCardByPath('/playlist/' + id)
+  let persistentEl
+
+  // Add the 'with-transition' class to the card that is transitioning
+  // and save a reference to any persistent elements (like video) if they exist
+  if (card) {
+    card.classList.add('with-transition')
+    persistentEl = getPersistentElement(card)
+  }
+
+  // Save the page scroll to restore it on the way back
+  prevPageScroll = document.documentElement.scrollTop
+
+  document.startViewTransition(() => {
+    updateTheDOMSomehow(data)
+    document.documentElement.scrollTop = 0
+
+    const persistentElContainer = getPersistentElementContainer()
+
+    // Place the persistent element into its container in the updated DOM
+    if (persistentEl && persistentElContainer) {
+      persistentElContainer.replaceChildren(persistentEl)
+    }
+  })
 })
 
-function handlePlaylistTransition(navigateEvent, toPath, fromPath) {
-  navigateEvent.intercept({
-    scroll: 'manual',
-    async handler() {
-      const response = await fetch('/fragments' + toPath)
-      const data = await response.text()
+router.on('/', async (match) => {
+  const response = await fetch('/fragments')
+  const data = await response.text()
 
-      if (!document.startViewTransition) {
-        updateTheDOMSomehow(data)
-        document.documentElement.scrollTop = 0
-        return
+  if (!document.startViewTransition) {
+    updateTheDOMSomehow(data)
+    return
+  }
+
+  // Save a reference to any persistent elements (like video) if they exist
+  const persistentEl = getPersistentElement()
+
+  document.startViewTransition(() => {
+    updateTheDOMSomehow(data)
+
+    console.log(lastMatch)
+
+    // Find the card that matches the playlist we're transitioning back from
+    const card = findCardByPath(`/${lastMatch?.url}`)
+
+    console.log(card)
+
+    // Add the 'with-transition' class to the transitioning card and
+    // place the persistent element its container in the updated DOM
+    if (card) {
+      card.classList.add('with-transition')
+      const persistentElContainer = getPersistentElementContainer(card)
+
+      if (persistentEl && persistentElContainer) {
+        persistentElContainer.replaceChildren(persistentEl)
       }
+    }
 
-      // Make sure there are no other cards with the 'with-transition' class
-      document
-        .querySelectorAll('.card')
-        .forEach((card) => card.classList.remove('with-transition'))
-
-      const card = findCardByPath(toPath)
-      let persistentEl
-
-      // Add the 'with-transition' class to the card that is transitioning
-      // and save a reference to any persistent elements (like video) if they exist
-      if (card) {
-        card.classList.add('with-transition')
-        persistentEl = getPersistentElement(card)
-      }
-
-      // Save the page scroll to restore it on the way back
-      prevPageScroll = document.documentElement.scrollTop
-
-      document.startViewTransition(() => {
-        updateTheDOMSomehow(data)
-        document.documentElement.scrollTop = 0
-
-        const persistentElContainer = getPersistentElementContainer()
-
-        // Place the persistent element into its container in the updated DOM
-        if (persistentEl && persistentElContainer) {
-          persistentElContainer.replaceChildren(persistentEl)
-        }
-      })
-    },
+    // Restore page scroll
+    if (prevPageScroll) {
+      document.documentElement.scrollTop = prevPageScroll
+    }
   })
-}
+})
 
-function handleHomeTransition(navigateEvent, toPath, fromPath) {
-  navigateEvent.intercept({
-    scroll: 'manual',
-    async handler() {
-      const response = await fetch('/fragments' + toPath)
-      const data = await response.text()
+// navigation.addEventListener('navigate', (navigateEvent) => {
+//   if (shouldNotIntercept(navigateEvent)) return
 
-      if (!document.startViewTransition) {
-        updateTheDOMSomehow(data)
-        return
-      }
+//   const toUrl = new URL(navigateEvent.destination.url)
+//   const toPath = toUrl.pathname
+//   const fromPath = location.pathname
 
-      // Save a reference to any persistent elements (like video) if they exist
-      const persistentEl = getPersistentElement()
+//   if (location.origin !== toUrl.origin) return
 
-      document.startViewTransition(() => {
-        updateTheDOMSomehow(data)
+//   if (toPath.indexOf('/playlist') === 0) {
+//     return handlePlaylistTransition(navigateEvent, toPath, fromPath)
+//   } else {
+//     return handleHomeTransition(navigateEvent, toPath, fromPath)
+//   }
+// })
 
-        // Find the card that matches the playlist we're transitioning back from
-        const card = findCardByPath(fromPath)
+// function handlePlaylistTransition(navigateEvent, toPath, fromPath) {
+//   navigateEvent.intercept({
+//     scroll: 'manual',
+//     async handler() {
+//       const response = await fetch('/fragments' + toPath)
+//       const data = await response.text()
 
-        // Add the 'with-transition' class to the transitioning card and
-        // place the persistent element its container in the updated DOM
-        if (card) {
-          card.classList.add('with-transition')
-          const persistentElContainer = getPersistentElementContainer(card)
+//       if (!document.startViewTransition) {
+//         updateTheDOMSomehow(data)
+//         document.documentElement.scrollTop = 0
+//         return
+//       }
 
-          if (persistentEl && persistentElContainer) {
-            persistentElContainer.replaceChildren(persistentEl)
-          }
-        }
+//       // Make sure there are no other cards with the 'with-transition' class
+//       document
+//         .querySelectorAll('.card')
+//         .forEach((card) => card.classList.remove('with-transition'))
 
-        // Restore page scroll
-        if (prevPageScroll) {
-          document.documentElement.scrollTop = prevPageScroll
-        }
-      })
-    },
-  })
-}
+//       const card = findCardByPath(toPath)
+//       let persistentEl
+
+//       // Add the 'with-transition' class to the card that is transitioning
+//       // and save a reference to any persistent elements (like video) if they exist
+//       if (card) {
+//         card.classList.add('with-transition')
+//         persistentEl = getPersistentElement(card)
+//       }
+
+//       // Save the page scroll to restore it on the way back
+//       prevPageScroll = document.documentElement.scrollTop
+
+//       document.startViewTransition(() => {
+//         updateTheDOMSomehow(data)
+//         document.documentElement.scrollTop = 0
+
+//         const persistentElContainer = getPersistentElementContainer()
+
+//         // Place the persistent element into its container in the updated DOM
+//         if (persistentEl && persistentElContainer) {
+//           persistentElContainer.replaceChildren(persistentEl)
+//         }
+//       })
+//     },
+//   })
+// }
+
+// function handleHomeTransition(navigateEvent, toPath, fromPath) {
+//   navigateEvent.intercept({
+//     scroll: 'manual',
+//     async handler() {
+//       const response = await fetch('/fragments' + toPath)
+//       const data = await response.text()
+
+//       if (!document.startViewTransition) {
+//         updateTheDOMSomehow(data)
+//         return
+//       }
+
+//       // Save a reference to any persistent elements (like video) if they exist
+//       const persistentEl = getPersistentElement()
+
+//       document.startViewTransition(() => {
+//         updateTheDOMSomehow(data)
+
+//         // Find the card that matches the playlist we're transitioning back from
+//         const card = findCardByPath(fromPath)
+
+//         // Add the 'with-transition' class to the transitioning card and
+//         // place the persistent element its container in the updated DOM
+//         if (card) {
+//           card.classList.add('with-transition')
+//           const persistentElContainer = getPersistentElementContainer(card)
+
+//           if (persistentEl && persistentElContainer) {
+//             persistentElContainer.replaceChildren(persistentEl)
+//           }
+//         }
+
+//         // Restore page scroll
+//         if (prevPageScroll) {
+//           document.documentElement.scrollTop = prevPageScroll
+//         }
+//       })
+//     },
+//   })
+// }
